ec1fbcb6bdae037a923d5f8fcd6f4e1ec792c7ab
herve.quiroz <herve.quiroz@localhost>
2009-02-23 00:30:25 +0000

    Fix license headers

--- src/main/java/org/trancecode/xproc/binding/PipePortBinding.java
+++ src/main/java/org/trancecode/xproc/binding/PipePortBinding.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (C) 2008 TranceCode Software
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  *
  * $Id$
  */
@@ -23,9 +23,9 @@ import org.trancecode.log.Logger;
 import org.trancecode.log.LoggerHelpers;
 import org.trancecode.log.LoggerManager;
 import org.trancecode.xml.Location;
-import org.trancecode.xproc.EnvironmentPortBinding;
 import org.trancecode.xproc.Environment;
 import org.trancecode.xproc.EnvironmentPort;
+import org.trancecode.xproc.EnvironmentPortBinding;
 import org.trancecode.xproc.PortBinding;
 import org.trancecode.xproc.PortReference;
 
