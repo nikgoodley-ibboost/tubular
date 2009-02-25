a72291e7d856cc072f0a102b35836d8952afd7bb
herve.quiroz <herve.quiroz@localhost>
2009-02-25 23:19:34 +0000

    Add all missing step constants

--- src/main/java/org/trancecode/xproc/XProcSteps.java
+++ src/main/java/org/trancecode/xproc/XProcSteps.java
@@ -30,17 +30,56 @@ import net.sf.saxon.s9api.QName;
  */
 public interface XProcSteps extends XProcNamespaces
 {
+	// Core steps
 	QName CHOOSE = NAMESPACE_XPROC.newSaxonQName("choose");
-	QName COUNT = NAMESPACE_XPROC.newSaxonQName("count");
 	QName FOR_EACH = NAMESPACE_XPROC.newSaxonQName("for-each");
 	QName GROUP = NAMESPACE_XPROC.newSaxonQName("group");
-	QName IDENTITY = NAMESPACE_XPROC.newSaxonQName("identity");
-	QName LOAD = NAMESPACE_XPROC.newSaxonQName("load");
 	QName OTHERWISE = NAMESPACE_XPROC.newSaxonQName("otherwise");
 	QName PIPELINE = NAMESPACE_XPROC.newSaxonQName("pipeline");
-	QName STORE = NAMESPACE_XPROC.newSaxonQName("store");
 	QName TRY = NAMESPACE_XPROC.newSaxonQName("try");
 	QName WHEN = NAMESPACE_XPROC.newSaxonQName("when");
-	QName XSL_FORMATTER = NAMESPACE_XPROC.newSaxonQName("xsl-formatter");
+
+	// Required steps
+	QName ADD_ATTRIBUTE = NAMESPACE_XPROC.newSaxonQName("add-attribute");
+	QName ADD_XML_BASE = NAMESPACE_XPROC.newSaxonQName("add-xml-base");
+	QName COMPARE = NAMESPACE_XPROC.newSaxonQName("compare");
+	QName COUNT = NAMESPACE_XPROC.newSaxonQName("count");
+	QName DELETE = NAMESPACE_XPROC.newSaxonQName("delete");
+	QName DIRECTORY_LIST = NAMESPACE_XPROC.newSaxonQName("directory-list");
+	QName ERROR = NAMESPACE_XPROC.newSaxonQName("error");
+	QName ESCAPE_MARKUP = NAMESPACE_XPROC.newSaxonQName("escape-markup");
+	QName FILTER = NAMESPACE_XPROC.newSaxonQName("filter");
+	QName HTTP_REQUEST = NAMESPACE_XPROC.newSaxonQName("http-request");
+	QName IDENTITY = NAMESPACE_XPROC.newSaxonQName("identity");
+	QName INSERT = NAMESPACE_XPROC.newSaxonQName("insert");
+	QName LABEL_ELEMENT = NAMESPACE_XPROC.newSaxonQName("label-element");
+	QName LOAD = NAMESPACE_XPROC.newSaxonQName("load");
+	QName MAKE_ABSOLUTE_URIS = NAMESPACE_XPROC.newSaxonQName("make-absolute-uris");
+	QName NAMESPACE_RENAME = NAMESPACE_XPROC.newSaxonQName("namespace-rename");
+	QName PACK = NAMESPACE_XPROC.newSaxonQName("pack");
+	QName PARAMETERS = NAMESPACE_XPROC.newSaxonQName("parameters");
+	QName RENAME = NAMESPACE_XPROC.newSaxonQName("rename");
+	QName REPLACE = NAMESPACE_XPROC.newSaxonQName("replace");
+	QName SET_ATTRIBUTES = NAMESPACE_XPROC.newSaxonQName("set-attributes");
+	QName SINK = NAMESPACE_XPROC.newSaxonQName("sink");
+	QName SPLIT_SEQUENCE = NAMESPACE_XPROC.newSaxonQName("split-sequence");
+	QName STORE = NAMESPACE_XPROC.newSaxonQName("store");
+	QName STRING_REPLACE = NAMESPACE_XPROC.newSaxonQName("string-replace");
+	QName UNESCAPE_MARKUP = NAMESPACE_XPROC.newSaxonQName("unescape-markup");
+	QName UNWRAP = NAMESPACE_XPROC.newSaxonQName("unwrap");
+	QName WRAP = NAMESPACE_XPROC.newSaxonQName("wrap");
+	QName XINXLUDE = NAMESPACE_XPROC.newSaxonQName("xinclude");
 	QName XSLT = NAMESPACE_XPROC.newSaxonQName("xslt");
+
+	// Optional steps
+	QName EXEC = NAMESPACE_XPROC.newSaxonQName("exec");
+	QName HASH = NAMESPACE_XPROC.newSaxonQName("hash");
+	QName UUID = NAMESPACE_XPROC.newSaxonQName("uuid");
+	QName VALIDATE_WITH_RELANXNG = NAMESPACE_XPROC.newSaxonQName("validate-with-relaxng");
+	QName VALIDATE_WITH_SCHEMATRON = NAMESPACE_XPROC.newSaxonQName("validate-with-schematron");
+	QName VALIDATE_WITH_SCHEMA = NAMESPACE_XPROC.newSaxonQName("validate-with-xml-schema");
+	QName WWW_FORM_URL_DECODE = NAMESPACE_XPROC.newSaxonQName("www-form-url-decode");
+	QName WWW_FORM_URL_ENCODE = NAMESPACE_XPROC.newSaxonQName("www-form-url-encode");
+	QName XQUERY = NAMESPACE_XPROC.newSaxonQName("xquery");
+	QName XSL_FORMATTER = NAMESPACE_XPROC.newSaxonQName("xsl-formatter");
 }
