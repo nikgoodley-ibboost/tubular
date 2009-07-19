8f3be8b8f292a39a70b19c7120413dbe20b15d7a
herve.quiroz <herve.quiroz@localhost>
2009-07-19 21:12:33 +0000

    Catalog is now a ForwardingCatalog and only acts as a facade to the functional logic

--- src/main/java/org/trancecode/xml/catalog2/Catalog.java
+++ src/main/java/org/trancecode/xml/catalog2/Catalog.java
@@ -21,50 +21,27 @@ package org.trancecode.xml.catalog2;
 
 import org.trancecode.annotation.Nullable;
 import org.trancecode.annotation.ReturnsNullable;
-import org.trancecode.core.AbstractImmutableObject;
-import org.trancecode.core.function.TubularFunctions;
 
 import java.net.URI;
 
 import com.google.common.base.Function;
-import com.google.common.base.Predicates;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 
 
 /**
  * @author Herve Quiroz
  * @version $Revision$
  */
-public final class Catalog extends AbstractImmutableObject implements Function<CatalogQuery, URI>
+public final class Catalog extends ForwardingCatalog implements Function<CatalogQuery, URI>
 {
-	private final Iterable<Function<CatalogQuery, URI>> catalogEntries;
-
-
-	public static Catalog newCatalog(final Iterable<Function<CatalogQuery, URI>> catalogEntries)
+	public static Catalog newCatalog(final Function<CatalogQuery, URI> catalogFunction)
 	{
-		return new Catalog(ImmutableList.copyOf(catalogEntries));
+		return new Catalog(catalogFunction);
 	}
 
 
-	public static Catalog newCatalog(final Function<CatalogQuery, URI>... catalogEntries)
+	private Catalog(final Function<CatalogQuery, URI> catalog)
 	{
-		return newCatalog(ImmutableList.of(catalogEntries));
-	}
-
-
-	private static Catalog newCatalog(
-		final Iterable<Function<CatalogQuery, URI>> catalogEntries1,
-		final Iterable<Function<CatalogQuery, URI>> catalogEntries2)
-	{
-		return newCatalog(Iterables.concat(catalogEntries1, catalogEntries2));
-	}
-
-
-	private Catalog(final Iterable<Function<CatalogQuery, URI>> catalogEntries)
-	{
-		super(catalogEntries);
-		this.catalogEntries = catalogEntries;
+		super(catalog);
 	}
 
 
@@ -74,38 +51,5 @@ public final class Catalog extends AbstractImmutableObject implements Function<C
 		@Nullable final String base)
 	{
 		return apply(CatalogQuery.newInstance(publicId, systemId, href, base));
-	}
-
-
-	@Override
-	public URI apply(final CatalogQuery query)
-	{
-		final Function<Function<CatalogQuery, URI>, URI> applyFunction = TubularFunctions.applyTo(query);
-		final Iterable<URI> results = Iterables.transform(catalogEntries, applyFunction);
-		return Iterables.find(results, Predicates.notNull());
-	}
-
-
-	public Catalog override(final Function<CatalogQuery, URI>... newCatalogEntries)
-	{
-		return override(ImmutableList.of(newCatalogEntries));
-	}
-
-
-	public Catalog override(final Iterable<Function<CatalogQuery, URI>> newCatalogEntries)
-	{
-		return newCatalog(newCatalogEntries, catalogEntries);
-	}
-
-
-	public Catalog addEntries(final Function<CatalogQuery, URI>... newCatalogEntries)
-	{
-		return addEntries(ImmutableList.of(newCatalogEntries));
-	}
-
-
-	public Catalog addEntries(final Iterable<Function<CatalogQuery, URI>> newCatalogEntries)
-	{
-		return newCatalog(catalogEntries, newCatalogEntries);
 	}
 }
