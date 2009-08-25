d74c96c5573c688e26ef9c088047f7384582919e
Herve Quiroz <Herve Quiroz@localhost>
2009-08-25 08:33:50 +0200

    Only build the list of step processors once

--- src/main/java/org/trancecode/xproc/PipelineFactory.java
+++ src/main/java/org/trancecode/xproc/PipelineFactory.java
@@ -66,7 +66,7 @@ public class PipelineFactory
 
 	private static final Map<QName, Step> CORE_LIBRARY = getCoreLibrary();
 	private static Map<QName, Step> defaultLibrary;
-	private static final Map<QName, StepProcessor> DEFAULT_PROCESSORS = getDefaultProcessors();
+	private static Map<QName, StepProcessor> defaultProcessors;
 
 	private Processor processor = new Processor(false);
 	private URIResolver uriResolver = processor.getUnderlyingConfiguration().getURIResolver();
@@ -141,56 +141,61 @@ public class PipelineFactory
 
 	private static Map<QName, StepProcessor> getDefaultProcessors()
 	{
-		final Map<QName, StepProcessor> processors = Maps.newHashMap();
+		if (defaultProcessors == null)
+		{
+			final Map<QName, StepProcessor> processors = Maps.newHashMap();
 
-		// Required steps
-		// TODO
-		processors.put(XProcSteps.COUNT, CountStepProcessor.INSTANCE);
-		processors.put(XProcSteps.IDENTITY, IdentityStepProcessor.INSTANCE);
-		processors.put(XProcSteps.LOAD, LoadStepProcessor.INSTANCE);
-		processors.put(XProcSteps.STORE, StoreStepProcessor.INSTANCE);
-		processors.put(XProcSteps.XSLT, XsltStepProcessor.INSTANCE);
+			// Required steps
+			// TODO
+			processors.put(XProcSteps.COUNT, CountStepProcessor.INSTANCE);
+			processors.put(XProcSteps.IDENTITY, IdentityStepProcessor.INSTANCE);
+			processors.put(XProcSteps.LOAD, LoadStepProcessor.INSTANCE);
+			processors.put(XProcSteps.STORE, StoreStepProcessor.INSTANCE);
+			processors.put(XProcSteps.XSLT, XsltStepProcessor.INSTANCE);
 
-		// Unsupported required steps
-		processors.put(XProcSteps.ADD_ATTRIBUTE, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.ADD_XML_BASE, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.COMPARE, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.DELETE, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.DIRECTORY_LIST, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.ERROR, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.ESCAPE_MARKUP, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.FILTER, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.HTTP_REQUEST, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.INSERT, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.LABEL_ELEMENT, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.MAKE_ABSOLUTE_URIS, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.NAMESPACE_RENAME, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.PACK, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.PARAMETERS, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.RENAME, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.REPLACE, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.SET_ATTRIBUTES, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.SINK, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.SPLIT_SEQUENCE, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.STRING_REPLACE, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.UNESCAPE_MARKUP, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.UNWRAP, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.WRAP, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.XINCLUDE, UNSUPPORTED_STEP_PROCESSOR);
+			// Unsupported required steps
+			processors.put(XProcSteps.ADD_ATTRIBUTE, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.ADD_XML_BASE, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.COMPARE, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.DELETE, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.DIRECTORY_LIST, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.ERROR, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.ESCAPE_MARKUP, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.FILTER, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.HTTP_REQUEST, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.INSERT, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.LABEL_ELEMENT, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.MAKE_ABSOLUTE_URIS, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.NAMESPACE_RENAME, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.PACK, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.PARAMETERS, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.RENAME, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.REPLACE, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.SET_ATTRIBUTES, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.SINK, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.SPLIT_SEQUENCE, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.STRING_REPLACE, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.UNESCAPE_MARKUP, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.UNWRAP, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.WRAP, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.XINCLUDE, UNSUPPORTED_STEP_PROCESSOR);
 
-		// Unsupported optional steps
-		processors.put(XProcSteps.EXEC, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.HASH, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.UUID, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.VALIDATE_WITH_RELANXNG, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.VALIDATE_WITH_SCHEMATRON, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.VALIDATE_WITH_SCHEMA, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.WWW_FORM_URL_DECODE, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.WWW_FORM_URL_ENCODE, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.XQUERY, UNSUPPORTED_STEP_PROCESSOR);
-		processors.put(XProcSteps.XSL_FORMATTER, UNSUPPORTED_STEP_PROCESSOR);
+			// Unsupported optional steps
+			processors.put(XProcSteps.EXEC, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.HASH, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.UUID, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.VALIDATE_WITH_RELANXNG, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.VALIDATE_WITH_SCHEMATRON, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.VALIDATE_WITH_SCHEMA, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.WWW_FORM_URL_DECODE, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.WWW_FORM_URL_ENCODE, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.XQUERY, UNSUPPORTED_STEP_PROCESSOR);
+			processors.put(XProcSteps.XSL_FORMATTER, UNSUPPORTED_STEP_PROCESSOR);
 
-		return ImmutableMap.copyOf(processors);
+			defaultProcessors = ImmutableMap.copyOf(processors);
+		}
+
+		return defaultProcessors;
 	}
 
 
