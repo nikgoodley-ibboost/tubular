bb462bb2ba6bfa87ace9d9bccbb2d59114fd65ab
Romain Deltour <rdeltour@gmail.com>
2010-04-29 18:35:30 +0200

    * Added a new XProcTests class that relies on the TestNG DataProvider mechanism to programmatically run all the XProc tests (rather than hard-coding the list of tests).  * Also added two DataProviders, respectively providing the test files URLs from the remote SVN   repository or a local directory.

---
+++ tubular-core/src/test/java/org/trancecode/xproc/XProcTests.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2010 Romain Deltour
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+package org.trancecode.xproc;
+
+import java.net.URL;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Romain Deltour
+ */
+public class XProcTests extends AbstractXProcTest
+{
+
+    @Test(dataProvider = "xprocTests", dataProviderClass = LocalXProcTestsProvider.class)
+    public void required(final URL url) throws Exception
+    {
+        test(url);
+    }
+
+    @Test(dataProvider = "xprocTests", dataProviderClass = LocalXProcTestsProvider.class)
+    public void optional(final URL url) throws Exception
+    {
+        test(url);
+    }
+
+}
