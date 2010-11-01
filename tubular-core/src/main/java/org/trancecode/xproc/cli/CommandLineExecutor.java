4c7d32d387f2da0a1c84bfd7eafa314153d4194a
czxdbj <czxdbj@localhost>
2010-11-01 11:20:46 +0100

    First upstream push for issue #4. Raises an exception.

--- tubular-core/src/main/java/org/trancecode/xproc/cli/CommandLineExecutor.java
+++ tubular-core/src/main/java/org/trancecode/xproc/cli/CommandLineExecutor.java
@@ -17,16 +17,101 @@
  */
 package org.trancecode.xproc.cli;
 
-import org.trancecode.xproc.Tubular;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.Properties;
+import javax.xml.transform.stream.StreamSource;
+import net.sf.saxon.s9api.QName;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.trancecode.xproc.Configuration;
+import org.trancecode.xproc.Pipeline;
+import org.trancecode.xproc.PipelineProcessor;
+import org.trancecode.xproc.PipelineResult;
+import org.trancecode.xproc.RunnablePipeline;
 
 /**
  * @author Herve Quiroz
+ * @author Torsten Knodt
  */
-public final class CommandLineExecutor
-{
-    public static void main(final String[] args)
-    {
-        System.err.println("Tubular version " + Tubular.version());
-        // TODO
+public final class CommandLineExecutor {
+
+    public static void main(final String[] args) {
+        final Options options = new Options();
+        final Option portBindingOption = new Option("b", "port-binding", true, "Passes a port binding to the given XProc pipeline");
+        final Option optionOption = new Option("o", "option", true, "Passes a option to the given XProc pipeline");
+        final Option paramOption = new Option("p", "param", true, "Passes a parameter to the given XProc pipeline");
+        final Option librariesOption = new Option("l", "library", true, "XProc pipeline library to load");
+        final Option xplOption = new Option("x", "xpl", true, "XProc pipeline to load and run");
+        final Option helpOption = new Option("h", "help", false, "Print help");
+
+        xplOption.setArgName("uri");
+        xplOption.setRequired(true);
+        xplOption.setType(URL.class);
+        portBindingOption.setArgName("name=uri");
+        portBindingOption.setArgs(2);
+        portBindingOption.setValueSeparator('=');
+        optionOption.setArgName("name=value");
+        optionOption.setArgs(2);
+        optionOption.setValueSeparator('=');
+        paramOption.setArgName("name=value");
+        paramOption.setArgs(2);
+        paramOption.setValueSeparator('=');
+        librariesOption.setArgName("uri");
+        librariesOption.setArgs(Option.UNLIMITED_VALUES);
+        librariesOption.setType(URL.class);
+        options.addOption(portBindingOption);
+        options.addOption(optionOption);
+        options.addOption(paramOption);
+        options.addOption(xplOption);
+        options.addOption(librariesOption);
+        options.addOption(helpOption);
+        final GnuParser parser = new GnuParser();
+        try {
+            final CommandLine commandLine = parser.parse(options, args);
+            if (commandLine.hasOption(helpOption.getOpt())) {
+                printHelp(options);
+                System.exit(0);
+            }
+            final Configuration configurationPipelineContext = new Configuration();
+            final PipelineProcessor pipelineProcessor = new PipelineProcessor(configurationPipelineContext);
+            for (final String library : librariesOption.getValues()) {
+                pipelineProcessor.buildPipelineLibrary(new StreamSource(library));
+            }
+            // configurationPipelineContext.registerStepProcessor(null);
+            final Pipeline buildPipeline = pipelineProcessor.buildPipeline(new StreamSource(xplOption.getValue()));
+            final RunnablePipeline runnablePipeline = buildPipeline.load();
+            final Properties portBindingProperties = commandLine.getOptionProperties(optionOption.getOpt());
+            for (final String portBindingName : portBindingProperties.stringPropertyNames()) {
+                final String portBindingValue = portBindingProperties.getProperty(portBindingName);
+                runnablePipeline.setPortBinding(portBindingName, new StreamSource(portBindingValue));
+            }
+            final Properties optionProperties = commandLine.getOptionProperties(optionOption.getOpt());
+            for (final String optionName : optionProperties.stringPropertyNames()) {
+                final String optionValue = optionProperties.getProperty(optionName);
+                runnablePipeline.withOption(new QName(optionName), optionValue);
+            }
+            final Properties paramProperties = commandLine.getOptionProperties(paramOption.getOpt());
+            for (final String paramName : paramProperties.stringPropertyNames()) {
+                final String paramValue = paramProperties.getProperty(paramName);
+                runnablePipeline.withOption(new QName(paramName), paramValue);
+            }
+            final PipelineResult run = runnablePipeline.run();
+        } catch (final ParseException ex) {
+            printHelp(options);
+            System.exit(1);
+        }
+    }
+
+    private static void printHelp(final Options options) {
+        final HelpFormatter helpFormatter = new HelpFormatter();
+        final PrintWriter printWriter = new PrintWriter(System.err);
+        helpFormatter.printHelp(printWriter, helpFormatter.getWidth(), "java -jar tubular-cli.jar", null, options, helpFormatter.getLeftPadding(), helpFormatter.getDescPadding(), null, true);
+        printWriter.flush();
+        printWriter.close();
     }
 }
