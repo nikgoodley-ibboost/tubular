10225a2d4d6d8559e9d678f7a277575626de1340
Torsten Knodt <torstenmarcoknodt@googlemail.com>
2010-11-05 10:42:41 +0100

    Introduced method for calling tubular during tests. Added invalid argument test.

--- tubular-cli/src/test/java/org/trancecode/xproc/cli/CommandLineExecutorTest.java
+++ tubular-cli/src/test/java/org/trancecode/xproc/cli/CommandLineExecutorTest.java
@@ -21,6 +21,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
@@ -37,27 +40,49 @@ public final class CommandLineExecutorTest
     @Test
     public void standardIOTest() throws IOException, InterruptedException, ExecutionException
     {
+        final String output = abstractTest(new String[]
+                {
+                    "--xpl", getClass().getResource("xproc-1.0.xml").toString()
+                }, 0);
+        Assert.assertTrue(!output.isEmpty(), "Did not find any output string");
+        Assert.assertTrue(output.contains("Inline XML conversion with inline XSLT using XProc"), "Did not find expected output string");
+    }
 
-        final ProcessBuilder processBuilder = new ProcessBuilder(new String[]
+    @Test
+    public void invalidArgumentShouldOutputHelpTest() throws IOException, InterruptedException, ExecutionException
+    {
+        final String output = abstractTest(new String[]
+                {
+                    "--blub", getClass().getResource("xproc-1.0.xml").toString()
+                }, 1);
+        Assert.assertTrue(!output.isEmpty(), "Did not find any output string");
+        Assert.assertTrue(output.contains("--xpl"), "Did not find expected output string");
+    }
+
+    private String abstractTest(final String[] commandLineArgs, final int expectedExitCode) throws IOException, InterruptedException, ExecutionException
+    {
+        final List<String> commandLineArgsList = new LinkedList<String>();
+        commandLineArgsList.addAll(Arrays.asList(new String[]
                 {
                     "java",
                     "-cp", System.getProperty("java.class.path"),
-                    CommandLineExecutor.class.getName(),
-                    "--xpl", getClass().getResource("xproc-1.0.xml").toString()
-                });
+                    CommandLineExecutor.class.getName()
+                }));
+        commandLineArgsList.addAll(Arrays.asList(commandLineArgs));
+        final ProcessBuilder processBuilder = new ProcessBuilder(commandLineArgsList.toArray(new String[0]));
+        processBuilder.redirectErrorStream(true);
         final Process process = processBuilder.start();
         process.getOutputStream().flush();
         process.getOutputStream().close();
         process.getErrorStream().close();
         final FutureTask<String> bufferedInputStreamReaderFutureTask = new FutureTask<String>(new BufferedInputStreamReaderCallable(process.getInputStream()));
         final FutureTask<String> bufferedErrorStreamReaderFutureTask = new FutureTask<String>(new BufferedInputStreamReaderCallable(process.getErrorStream()));
-        new Thread(bufferedInputStreamReaderFutureTask, "standardIOTest Output Handler").start();
-        new Thread(bufferedErrorStreamReaderFutureTask, "standardIOTest Error Handler").start();
+        new Thread(bufferedInputStreamReaderFutureTask, "Output Handler").start();
+        new Thread(bufferedErrorStreamReaderFutureTask, "Error Handler").start();
         final String output = bufferedInputStreamReaderFutureTask.get();
-        Assert.assertEquals(process.waitFor(), 0, "Checking exit value of tubular process");
+        Assert.assertEquals(process.waitFor(), expectedExitCode, "Checking exit value of tubular process");
         process.destroy();
-        Assert.assertTrue(!output.isEmpty(), "Did not find any output string");
-        Assert.assertTrue(output.contains("Inline XML conversion with inline XSLT using XProc"), "Did not find expected output string");
+        return output;
     }
 
     private class BufferedInputStreamReaderCallable implements Callable<String>
@@ -75,7 +100,8 @@ public final class CommandLineExecutorTest
         public final String call() throws Exception
         {
             String line = null;
-            while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null)
+            {
                 sb.append(line);
                 System.out.println(line);
             }
