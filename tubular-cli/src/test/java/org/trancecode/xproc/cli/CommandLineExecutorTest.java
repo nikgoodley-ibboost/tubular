67e2ec61a906714dc21472dc52898d1bc2884894
Herve Quiroz <Herve Quiroz@localhost>
2010-11-06 01:15:40 +0100

    Setup some test suite for the command line executor In each child directory, the test method will parse test.properties to find out about arguments, expected exit code (optional), file to use as stdin (optional), and file that contains the expected stdout content

--- tubular-cli/src/test/java/org/trancecode/xproc/cli/CommandLineExecutorTest.java
+++ tubular-cli/src/test/java/org/trancecode/xproc/cli/CommandLineExecutorTest.java
@@ -17,19 +17,34 @@
  */
 package org.trancecode.xproc.cli;
 
+import com.beust.jcommander.internal.Lists;
+
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
+import org.apache.commons.io.FileUtils;
+import org.custommonkey.xmlunit.XMLAssert;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.trancecode.AbstractTest;
+import org.trancecode.io.Files;
+import org.trancecode.io.IOUtil;
+import org.trancecode.logging.Logger;
 
 /**
  * Tests for {@link CommandLineExecutor}.
@@ -37,8 +52,10 @@ import org.testng.annotations.Test;
  * @author Herve Quiroz
  * @author Torsten Knodt
  */
-public final class CommandLineExecutorTest
+public final class CommandLineExecutorTest extends AbstractTest
 {
+    private static final Logger LOG = Logger.getLogger(CommandLineExecutorTest.class);
+
     @Test
     public void standardIOTest() throws IOException, InterruptedException, ExecutionException
     {
@@ -106,6 +123,102 @@ public final class CommandLineExecutorTest
             }
             br.close();
             return sb.toString();
+        }
+    }
+
+    public static final class TestProvider
+    {
+        @DataProvider(name = "testDirectories")
+        public static Object[][] listTests(final Method method) throws Exception
+        {
+            try
+            {
+                final List<Object[]> tests = Lists.newArrayList();
+
+                final File testRootDirectory = new File(CommandLineExecutorTest.class.getResource(
+                        "/" + CommandLineExecutorTest.class.getSimpleName() + "/root").toURI()).getParentFile();
+                LOG.info("testRootDirectory = {}", testRootDirectory);
+                assert testRootDirectory.isDirectory();
+                for (final File testDirectory : Files.listDirectories(testRootDirectory))
+                {
+                    final Properties properties = new Properties();
+                    properties.load(Files.newFileInputStream(new File(testDirectory, "test.properties")));
+                    final String[] args = properties.getProperty("args")
+                            .replace("${test.directory}", testDirectory.getAbsolutePath() + "/").split(" ");
+
+                    final InputStream stdin;
+                    if (properties.getProperty("stdin") != null)
+                    {
+                        final File stdinFile = new File(testDirectory, properties.getProperty("stdin"));
+                        stdin = Files.newFileInputStream(stdinFile);
+                    }
+                    else
+                    {
+                        stdin = IOUtil.newNullInputStream();
+                    }
+
+                    final String expectedStdout;
+                    if (properties.getProperty("stdout") != null)
+                    {
+                        final File stdoutFile = new File(testDirectory, properties.getProperty("stdout"));
+                        expectedStdout = FileUtils.readFileToString(stdoutFile);
+                    }
+                    else
+                    {
+                        expectedStdout = null;
+                    }
+
+                    final int expectedExitCode = Integer.parseInt(properties.getProperty("exit.code", "0"));
+
+                    tests.add(new Object[] { args, stdin, expectedStdout, expectedExitCode });
+                }
+
+                return tests.toArray(new Object[][] {});
+            }
+            catch (final Exception e)
+            {
+                e.printStackTrace();
+                throw e;
+            }
+        }
+    }
+
+    @Test(dataProvider = "testDirectories", dataProviderClass = TestProvider.class)
+    public void test(final String[] args, final InputStream stdin, final String expectedStdout,
+            final int expectedExitCode)
+    {
+        final ByteArrayOutputStream stdoutBytes = new ByteArrayOutputStream();
+        final PrintStream stdout = new PrintStream(stdoutBytes);
+
+        final int exitCode;
+        try
+        {
+            exitCode = new CommandLineExecutor().execute(args, stdin, stdout, System.err);
+        }
+        finally
+        {
+            stdout.close();
+        }
+        final String stdoutContent = stdoutBytes.toString();
+
+        Assert.assertEquals(exitCode, expectedExitCode);
+        if (expectedStdout != null)
+        {
+            assertXmlEquals(stdoutContent, expectedStdout);
+        }
+        // TODO handle stderr
+    }
+
+    private static void assertXmlEquals(final String actual, final String expected)
+    {
+        try
+        {
+            XMLUnit.setIgnoreWhitespace(true);
+            XMLAssert.assertXMLEqual(expected, actual);
+        }
+        catch (final Exception e)
+        {
+            throw new IllegalStateException(String.format("expected:\n%s\nactual:\n%s", expected, actual), e);
         }
     }
 }
