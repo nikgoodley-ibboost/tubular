5fcc4d05e67b3485bbded1cafac2c51d72347169
Herve Quiroz <Herve Quiroz@localhost>
2011-01-10 22:12:45 +0100

    Declare p:for-each/current port

--- tubular-core/src/main/java/org/trancecode/xproc/step/ForEachStepProcessor.java
+++ tubular-core/src/main/java/org/trancecode/xproc/step/ForEachStepProcessor.java
@@ -51,8 +51,9 @@ public final class ForEachStepProcessor extends AbstractCompoundStepProcessor im
     @Override
     public Step getStepDeclaration()
     {
-        final Iterable<Port> ports = ImmutableList.of(Port.newInputPort(XProcPorts.ITERATION_SOURCE).setSequence(true),
-                Port.newOutputPort(XProcPorts.RESULT).setSequence(true));
+        final Iterable<Port> ports = ImmutableList.of(Port.newInputPort(XProcPorts.ITERATION_SOURCE).setPrimary(true)
+                .setSequence(true), Port.newInputPort(XProcPorts.CURRENT), Port.newOutputPort(XProcPorts.RESULT)
+                .setSequence(true));
         return Step.newStep(XProcSteps.FOR_EACH, this, true).declarePorts(ports);
     }
 
