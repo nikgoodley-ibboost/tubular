5341bf25af780132f58d2a34a25516acac2c0526
Herve Quiroz <Herve Quiroz@localhost>
2011-01-11 07:45:10 +0100

    Clean-up obsolete tests

--- tubular-cli/src/test/java/org/trancecode/xproc/cli/CommandLineExecutorTest.java
+++ tubular-cli/src/test/java/org/trancecode/xproc/cli/CommandLineExecutorTest.java
@@ -19,21 +19,13 @@ package org.trancecode.xproc.cli;
 
 import com.beust.jcommander.internal.Lists;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
 
 import org.apache.commons.io.FileUtils;
 import org.custommonkey.xmlunit.XMLAssert;
@@ -55,76 +47,6 @@ import org.trancecode.logging.Logger;
 public final class CommandLineExecutorTest extends AbstractTest
 {
     private static final Logger LOG = Logger.getLogger(CommandLineExecutorTest.class);
-
-    @Test
-    public void standardIOTest() throws IOException, InterruptedException, ExecutionException
-    {
-        final String output = abstractTest(
-                new String[] { "--xpl", getClass().getResource("/org/trancecode/xproc/cli/xproc-1.0.xml").toString() },
-                0);
-        Assert.assertTrue(!output.isEmpty(), "Did not find any output string");
-        Assert.assertTrue(output.contains("Inline XML conversion with inline XSLT using XProc"),
-                "Did not find expected output string");
-    }
-
-    @Test
-    public void invalidArgumentShouldOutputHelpTest() throws IOException, InterruptedException, ExecutionException
-    {
-        final String output = abstractTest(
-                new String[] { "--blub", getClass().getResource("/org/trancecode/xproc/cli/xproc-1.0.xml").toString() },
-                1);
-        Assert.assertTrue(!output.isEmpty(), "Did not find any output string");
-        Assert.assertTrue(output.contains("--xpl"), "Did not find expected output string");
-    }
-
-    private String abstractTest(final String[] commandLineArgs, final int expectedExitCode) throws IOException,
-            InterruptedException, ExecutionException
-    {
-        final List<String> commandLineArgsList = new LinkedList<String>();
-        commandLineArgsList.addAll(Arrays.asList(new String[] { "java", "-cp", System.getProperty("java.class.path"),
-                CommandLineExecutor.class.getName() }));
-        commandLineArgsList.addAll(Arrays.asList(commandLineArgs));
-        final ProcessBuilder processBuilder = new ProcessBuilder(commandLineArgsList.toArray(new String[0]));
-        processBuilder.redirectErrorStream(true);
-        final Process process = processBuilder.start();
-        process.getOutputStream().flush();
-        process.getOutputStream().close();
-        process.getErrorStream().close();
-        final FutureTask<String> bufferedInputStreamReaderFutureTask = new FutureTask<String>(
-                new BufferedInputStreamReaderCallable(process.getInputStream()));
-        final FutureTask<String> bufferedErrorStreamReaderFutureTask = new FutureTask<String>(
-                new BufferedInputStreamReaderCallable(process.getErrorStream()));
-        new Thread(bufferedInputStreamReaderFutureTask, "Output Handler").start();
-        new Thread(bufferedErrorStreamReaderFutureTask, "Error Handler").start();
-        final String output = bufferedInputStreamReaderFutureTask.get();
-        Assert.assertEquals(process.waitFor(), expectedExitCode, "Checking exit value of tubular process");
-        process.destroy();
-        return output;
-    }
-
-    private class BufferedInputStreamReaderCallable implements Callable<String>
-    {
-        private final BufferedReader br;
-        private final StringBuilder sb = new StringBuilder();
-
-        public BufferedInputStreamReaderCallable(final InputStream inputStream)
-        {
-            br = new BufferedReader(new InputStreamReader(inputStream));
-        }
-
-        @Override
-        public final String call() throws Exception
-        {
-            String line = null;
-            while ((line = br.readLine()) != null)
-            {
-                sb.append(line);
-                System.out.println(line);
-            }
-            br.close();
-            return sb.toString();
-        }
-    }
 
     public static final class TestProvider
     {
