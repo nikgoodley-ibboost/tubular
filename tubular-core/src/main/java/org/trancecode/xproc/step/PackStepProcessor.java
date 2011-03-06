e8ab0f62b3df0d5b4f1d2c6e72044dd2cb296c80
Herve Quiroz <Herve Quiroz@localhost>
2011-03-06 17:34:56 +0100

    Re-format

--- tubular-core/src/main/java/org/trancecode/xproc/step/PackStepProcessor.java
+++ tubular-core/src/main/java/org/trancecode/xproc/step/PackStepProcessor.java
@@ -21,6 +21,10 @@ package org.trancecode.xproc.step;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+
+import java.util.Iterator;
+import java.util.Set;
+
 import net.sf.saxon.s9api.QName;
 import net.sf.saxon.s9api.XdmNode;
 import net.sf.saxon.s9api.XdmNodeKind;
@@ -29,9 +33,6 @@ import org.trancecode.xml.saxon.SaxonAxis;
 import org.trancecode.xml.saxon.SaxonBuilder;
 import org.trancecode.xproc.port.XProcPorts;
 import org.trancecode.xproc.variable.XProcOptions;
-
-import java.util.Iterator;
-import java.util.Set;
 
 /**
  * {@code p:pack}.
